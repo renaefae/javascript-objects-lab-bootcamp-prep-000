83baf0ba0ebf92231f1744e1ab191f80ad191b38
Naenae L <oneprincesslea@gmail.com>
2018-11-13 17:59:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 var recipes = { quiche: 'eggs' }
-
 var newRecipe = Object.assign({}, recipes)
-
 newRecipe
 console.log(newRecipe);
-
 delete newRecipe.quiche ;
-
 newRecipe;
  console.log(newRecipe);
 recipes;
-console.log(recipes);
+console.log(recipes); 
